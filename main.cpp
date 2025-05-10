c29e7fcec7ce46bfcf416cfc4afade21ab4a7d68
DaniloButtu <s311216@studenti.polito.it>
2025-05-10 17:29:23 +0200

    Add in main input data by terminal

--- main.cpp
+++ main.cpp
@@ -6,9 +6,20 @@
 using namespace std;
 using namespace PolyhedronLibrary;
 
-int main()
+int main(int argc, char* argv[])
 {   
-    cout<<"Il progetto compila!"<<endl;
-    cout<<"bdkjbjkdk"<<endl;
+    if(argc < 5)
+    {
+        cerr<<"usage: "<<argv[0]<<" <p> <q> <b> <c> [<begin id vertex> <end id vertex>]"<<endl;
+        return 1;
+    }
+    
+    const unsigned int p = stoi(argv[1]);
+    const unsigned int q = stoi(argv[2]);
+    const unsigned int b = stoi(argv[3]);
+    const unsigned int c = stoi(argv[4]);
+
+    cout<<"p, q, b, c: "<<p<<' '<<q<<' '<<b<<' '<<c<<endl;
+    
     return 0;
 }
--- src/Utils.hpp
+++ src/Utils.hpp
@@ -8,16 +8,9 @@ using namespace std;
 namespace PolyhedronLibrary{
 
 /*Function to import a platonic solid*/
-bool Import_solid(string &solid_name, Polyhedron &P);
+bool Import_platonic_solid(unsigned int &p, unsigned int &q, Polyhedron &P);
 
-/*Import the Cell0D properties from Cell0Ds.txt file*/
-bool import_cell0Ds(string &solid_name, Polyhedron &P);
-
-/*Import the Cell1D properties from Cell1Ds.txt file*/
-bool import_cell1Ds(string &solid_name, Polyhedron &P);
-
-/*Import the Cell2D properties from Cell2Ds.txt file*/
-bool import_cell2Ds(string &solid_name, Polyhedron &P);
+bool Export_polyhedron(Polyhedron &P);
 
 }
 
