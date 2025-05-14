c7560903e9ba80ab293bb539456cd3301906266d
Nicola Fialà <s307179@studenti.polito.it>
2025-05-14 17:24:26 +0200

    corrected

--- src/funzioni_di_prova.cpp
+++ src/funzioni_di_prova.cpp
@@ -1,26 +1,33 @@
 // prova funzione di proiezione del solido su una sfera centrata nell'origine e di raggio unitario
 
+#include "Polyhedron.hpp"
+#include <Eigen/Dense>
 
+namespace PolyhedronLibrary{
  void project_points_onto_sphere(Polyhedron &P)
 	 {
 	// salvo il numero di vertici
 	MatrixXd &S = P.cell0Ds_coordinates;
-	double &n = P.numcell0ds; 
+	int n = P.num_cell0Ds; 
 	//calcolo il baricentro del solido
-	barycenter = VectorXi: Zero(3);
-	for (i =0; i<n; i++)
-	{{for (j =0; j<3; j++)
+	VectorXd barycenter =  VectorXd::Zero(3);
+	for (int i =0; i<n; i++)
+	{{for (int j =0; j<3; j++)
 	barycenter(j) += S(i,j)/n;}
 	 }
 	 // traslo rispetto al baricentro
 	 for(size_t h = 0; h<n;h++)
-	 {{for (j =0; j<3; j++)
-	 S(h,j) -=barycenter(j);}};
+	 {
+		 for (int j =0; j<3; j++)
+		 {S(h,j) -=barycenter(j);}
+ 
+	}
 	
 	// proietto
-	for (i = 0; i<n; i++)
-	{double norm = sqrt(S(i,0)^2+S(i,1)^2+S(i,2)^2);
-		for (j=0;j<3;j++)
-		{S(i,j) /= abs(norm)}}	;
+	for (int i = 0; i<n; i++)
+	{double norm = sqrt(S(i,0)*S(i,0)+S(i,1)*S(i,1)+S(i,2)*S(i,2));
+		for (int j=0;j<3;j++)
+		{S(i,j) /= abs(norm);}
+	}	;
 	};
- 
+}
