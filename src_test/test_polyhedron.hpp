aaae45093b10c05dc8b4f0ef723cd4155dd933be
Nicola Fialà <s307179@studenti.polito.it>
2025-05-14 17:43:28 +0200

    added correct one

--- src_test/test_polyhedron.hpp
+++ src_test/test_polyhedron.hpp
@@ -1,4 +1,96 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+
 #include <gtest/gtest.h>
+#include "funzioni_di_prova.cpp"
 #include "Polyhedron.hpp"
+using namespace Eigen;
+namespace PolyhedronLibrary {
+	// cubo già normalizzato
+TEST(TestPolyhedron, TestProjection)
+{	
+    Polyhedron solid;
+	int n=8;
+	solid.num_cell0Ds = n;
+	MatrixXd mat(n,3);
+	mat << 0.0, 0.0, 1.224745,
+	1.154701, 0.0, 0.4082483,
+	-0.5773503, 1.0, 0.4082483,
+	-0.5773503, -1.0, 0.4082483,
+	0.5773503, 1.0, -0.4082483,
+	0.5773503, -1.0, -0.4082483,
+	-1.154701, 0.0, -0.4082483,		
+	0.0, 0.0, -1.224745;
+	solid.cell0Ds_coordinates = mat.transpose();
+    project_points_onto_sphere(solid);
+    Polyhedron projectedSolid;
+	projectedSolid.num_cell0Ds = n;
+	MatrixXd mat2(n,3);
+	mat2 << 0.0, 0.0, 1.224745,
+	1.154701, 0.0, 0.4082483,
+	-0.5773503, 1.0, 0.4082483,
+	-0.5773503, -1.0, 0.4082483,
+	0.5773503, 1.0, -0.4082483,
+	0.5773503, -1.0, -0.4082483,
+	-1.154701, 0.0, -0.4082483,		
+	0.0, 0.0, -1.224745;
+	projectedSolid.cell0Ds_coordinates = mat2.transpose();
+	bool Test = true;
+	if (!solid.cell0Ds_coordinates.isApprox(projectedSolid.cell0Ds_coordinates))
+		Test = false;
+	EXPECT_EQ(Test, 0);
+}
+
+
+// tetraedro da normalizzare
+TEST(TestPolyhedron, TestProjection2)
+{	
+    Polyhedron solid;
+	int n=4;
+	solid.num_cell0Ds = n;
+	MatrixXd mat(n,3);
+	mat << 2.0, 2.0, 2.0,
+            -2.0, -2.0, 2.0,
+            -2.0, 2.0, -2.0,
+             2.0, -2.0, -2.0;
+	solid.cell0Ds_coordinates = mat.transpose();
+    project_points_onto_sphere(solid);
+    Polyhedron projectedSolid;
+	projectedSolid.num_cell0Ds = n;
+	MatrixXd mat2(n,3);
+	mat2 << 0.577, 0.577, 0.577,
+	-0.577, -0.577, 0.577,
+	-0.577, 0.577, -0.577,
+	0.577, -0.577, -0.577;
+	projectedSolid.cell0Ds_coordinates = mat2.transpose();
+	bool Test = true;
+	if (!solid.cell0Ds_coordinates.isApprox(projectedSolid.cell0Ds_coordinates))
+		Test = false;
+	EXPECT_EQ(Test, 0);
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
